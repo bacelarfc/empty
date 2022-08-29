9cd96d41bfa194f81d3a26043653da97e8a8cbdc
bacelarfc <fernandacbacelar@gmail.com>
2022-08-29 14:23:15 +0200

    test

---
+++ GitHubActionsApp/src/main/java/com/example/githubactionsapp/GitHubActionsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActionsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubActionsAppApplication.class, args);
+    }
+
+}
---
+++ GitHubActionsApp/src/test/java/com/example/githubactionsapp/GitHubActionsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubActionsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
